36d5125bee3ee5ddf2299675dc9dec51af22a2a9
StamplerJ <jann.luellmann@web.de>
2018-02-17 13:43:14 +0100

    Aaron introduction

--- core/src/com/beemelonstudio/lanemania/entities/obstacles/TriangleObstacle.java
+++ core/src/com/beemelonstudio/lanemania/entities/obstacles/TriangleObstacle.java
@@ -1,8 +1,11 @@
 package com.beemelonstudio.lanemania.entities.obstacles;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.PolygonRegion;
 import com.badlogic.gdx.graphics.g2d.PolygonSpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.EarClippingTriangulator;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
@@ -24,13 +27,28 @@ public class TriangleObstacle extends Entity {
     public TriangleObstacle(Body body) {
         super(body);
 
-        calculateSizes();
+        //calculateSizes();
+
+        vertices = new float[]{0,0,-0.197f, 0.229f, 0.021f, -0.39f};
 
         EarClippingTriangulator triangulator = new EarClippingTriangulator();
         triangles = triangulator.computeTriangles(vertices);
 
-        textureRegion = textureAtlas.findRegion("rectangle");
+        textureRegion = textureAtlas.findRegion("triangle");
+        /*Pixmap pix = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+        pix.setColor(0xFF3369FF); // DE is red, AD is green and BE is blue.
+        pix.fill();
+        Texture texturee = new Texture(pix);
+        TextureRegion textureRegion = new TextureRegion(texturee);*/
         texture = new PolygonRegion(textureRegion, vertices, triangles.toArray());
+
+        for(int i = 0; i < vertices.length; i++) {
+            Gdx.app.log("Verts", vertices[i]+" / "+texture.getVertices()[i]);
+        }
+
+        for(int j = 0; j < triangles.size; j++) {
+            Gdx.app.log("Triangle", triangles.toArray()[j]+"");
+        }
     }
 
     @Override
@@ -44,6 +62,19 @@ public class TriangleObstacle extends Entity {
     public void draw(PolygonSpriteBatch batch) {
 
         batch.draw(texture, x, y);
+
+//        batch.draw(textureRegion, x, y, 0.1f, 0.1f);
+  //      batch.draw(textureRegion, x-0.197f, y-0.229f, 0.1f, 0.1f);
+    //    batch.draw(textureRegion, x+0.021f, y-0.39f, 0.1f, 0.1f);
+
+        /*
+        V: 0.44630462
+        V: 0.5905334
+        V: 0.4250521
+        V: 0.9808084
+        V: 0.22798271
+        V: 0.7508938
+         */
     }
 
     /**
@@ -63,16 +94,16 @@ public class TriangleObstacle extends Entity {
             shape.getVertex(i, verts.get(i));
         }
 
-        vertices = new float[verts.size * 2];
-        int j = 0;
+        vertices = new float[verts.size * 2 + 2];
+        vertices[0] = 0;
+        vertices[1] = 0;
+        int j = 2;
         for(int i = 0; i < verts.size; i++) {
-            vertices[j] = verts.get(i).x > 0 ? x - verts.get(i).x : x + verts.get(i).x;
-            vertices[j+1] = verts.get(i).y > 0 ? y - verts.get(i).y : y + verts.get(i).y;
+            vertices[j] = verts.get(i).x;
+            vertices[j+1] = verts.get(i).y;
 
             j+=2;
         }
-
-        Gdx.app.log("Pos", x + " - " + y);
 
         for(int t = 0; t < vertices.length; t++) {
             Gdx.app.log("V", vertices[t]+"");
