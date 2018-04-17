21c1b451753f7beb4afa65d8ed8362516e054235
Cedric <cedric@beemelonstudio.com>
2018-04-17 18:32:31 +0200

    mutebutton image

--- core/src/com/beemelonstudio/lanemania/screens/SettingsScreen.java
+++ core/src/com/beemelonstudio/lanemania/screens/SettingsScreen.java
@@ -1,14 +1,18 @@
 package com.beemelonstudio.lanemania.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.beemelonstudio.lanemania.LaneMania;
 import com.beemelonstudio.lanemania.screens.custombuttons.BmsImageButton;
+import com.beemelonstudio.lanemania.utils.assets.Assets;
 
 /**
  * Created by Cedric on 06.03.2018.
@@ -36,10 +40,11 @@ public class SettingsScreen extends GameScreen {
         super.show();
 
         isBackgroundDrawing = true;
+        textureAtlas = (TextureAtlas) Assets.get("general-theme");
 
-        muteIcon = textureAtlas.findRegion("circle");
-        leftArrowIcon = textureAtlas.findRegion("ball");
-        rightArrowIcon = textureAtlas.findRegion("ball");
+        muteIcon = textureAtlas.findRegion("mute_button_on");
+        leftArrowIcon = textureAtlas.findRegion("arrow_left");
+        rightArrowIcon = textureAtlas.findRegion("arrow_right");
 
         table = new Table();
         table.setFillParent(true);
@@ -60,7 +65,8 @@ public class SettingsScreen extends GameScreen {
 
     public void createMenu(){
 
-        BmsImageButton muteButton = new BmsImageButton(skin, muteIcon);
+        BmsImageButton muteButton = new BmsImageButton(skin, textureAtlas.findRegion("mute_button_on"));
+        muteButton.getStyle().imageChecked = new TextureRegionDrawable(textureAtlas.findRegion("mute_button_off"));
         BmsImageButton leftArrowButton = new BmsImageButton(skin, leftArrowIcon);
         BmsImageButton rightArrowButton = new BmsImageButton(skin, rightArrowIcon);
         TextButton returnButton = new TextButton("Return", skin);
@@ -83,10 +89,12 @@ public class SettingsScreen extends GameScreen {
                     volume = 0.0f;
                     backgroundMusic.setVolume(volume);
                     muted = true;
+
                 } else {
                     volume = 1.0f;
                     backgroundMusic.setVolume(volume);
                     muted = false;
+
                 }
             }
         });
