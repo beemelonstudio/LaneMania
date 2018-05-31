9fc0008ae362d78138bedb0deb927a7b903ba1cb
Cedric <cedric@beemelonstudio.com>
2018-05-31 19:52:29 +0200

    started work on "endscreen"

--- core/src/com/beemelonstudio/lanemania/screens/MapSelectionScreen.java
+++ core/src/com/beemelonstudio/lanemania/screens/MapSelectionScreen.java
@@ -31,7 +31,7 @@ public class MapSelectionScreen extends GameScreen {
     private TextureRegion leftArrowIcon;
     private TextureRegion rightArrowIcon;
 
-    public MapSelectionScreen(ClickListener game) {
+    public MapSelectionScreen(LaneMania game) {
         super(game);
     }
 
