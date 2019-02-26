eed67f8ceb06bee3bc89a7689153afebbb981e99
Jann Lüllmann <jann.luellmann@web.de>
2019-02-26 23:05:27 +0100

    - Java Update

--- desktop/src/com/beemelonstudio/lanemania/desktop/DesktopLauncher.java
+++ desktop/src/com/beemelonstudio/lanemania/desktop/DesktopLauncher.java
@@ -7,8 +7,8 @@ import com.beemelonstudio.lanemania.LaneMania;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.width = (int) (720f / 1.5f);
-        config.height = (int) (1280f / 1.5f);
+        config.width = (int) (400f / 1f);
+        config.height = (int) (711f / 1f);
         config.title = "LaneMania";
 
 		new LwjglApplication(new LaneMania(), config);
